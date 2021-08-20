497473c6ef2901938f00b1cf57580a2975401256
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-19 22:32:52 -0300

    Alteração em banco

--- Curso_emVídeo/Aula6.sql
+++ Curso_emVídeo/Aula6.sql
@@ -1,1 +1,8 @@
 -- Hoje vamos aprender os comandos alter table e drop table
+-- Banco de dados -> tabelas -> campos (column) 
+-- EX: 
+desc pessoas; -- Este comando descreve a tabela pessoas, equivalente a describe; 
+-- Como adicionar uma coluna? 
+alter table pessoas
+add column profissao varchar(10); 
+ 
