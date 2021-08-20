a76717f76ef96636a7505a6d90f7c1545f384a7f
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-19 22:43:18 -0300

    Update Aula6.sql

--- Curso_emVídeo/Aula6.sql
+++ Curso_emVídeo/Aula6.sql
@@ -1,8 +1,22 @@
 -- Hoje vamos aprender os comandos alter table e drop table
 -- Banco de dados -> tabelas -> campos (column) 
 -- EX: 
+use cadastro;
 desc pessoas; -- Este comando descreve a tabela pessoas, equivalente a describe; 
 -- Como adicionar uma coluna? 
 alter table pessoas
 add column profissao varchar(10); 
+select * from pessoas; -- Você repara que a coluna profissão aparece como nula; 
+-- A profissão foi adicionada como último campo; 
+-- Para alterar a posição, primeiro precisamos eliminar ela; 
+-- Com o comando: 
+alter table pessoas 
+drop column profissao;
+-- Agora escolhemos a nova posição!
+alter table pessoas 
+add column profissao varchar(10) after nome; -- não existe before. 
+select * from pessoas;
+-- Se desejas que a nova coluna seja o primeiro campo da tabela, deves: 
+alter table pessoas 
+add column cpf int first;
  
