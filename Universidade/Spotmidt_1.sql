c19c3aefdc6366356eaed0dc5c7ad720b84a01f9
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-27 16:53:12 -0300

    Mudanças de nomes.
    Com a ajuda do professor de programação orientada a objetos, consegui resolver alguns problemas de implementação, bem como padronizar algumas tabelas pensando nos nomes de seus atirbutos.

--- Universidade/Spotmidt_1.sql
+++ Universidade/Spotmidt_1.sql
@@ -11,10 +11,9 @@ localizacao varchar(40) not null,
 país varchar(25) not null default "Brasil",
 nome varchar(25) not null,
 sobrenome varchar(25) not null,
-nascimento date not null,
+nascimento date not null ,
 primary key(cod_user)
 )default charset=utf8; 
-
 
 create table Tb_Artista(
 cod_bandaartista int auto_increment not null unique,
@@ -22,12 +21,14 @@ nome_bandaartista varchar(40) not null,
 país_bandaartista varchar(40) not null default "Brasil",
 primary key(cod_bandaartista)
 )default charset=utf8;
+use spotmidt;
 
-create table Tb_Musicas(
-cod_musicas int auto_increment not null unique,
+create table Tb_Musica(
+cod_musica int auto_increment not null unique,
 nome_musica varchar(40) not null,
 duracao_musica time,
 letra_música varchar(400) default "Indisponível",
-primary key (cod_musicas)
+primary key (cod_musica)
 )default charset=utf8;
+
 
