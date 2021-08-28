77086e3f5ad1b774bd42e56589c6bf5770b1e96c
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-28 00:14:35 -0300

    Banco Spotmidt sendo criado

--- Universidade/Spotmidt_1.sql
+++ Universidade/Spotmidt_1.sql
@@ -31,4 +31,50 @@ letra_música varchar(400) default "Indisponível",
 primary key (cod_musica)
 )default charset=utf8;
 
+use spotmidt;
+
+create table Tb_Album(
+cod_album int auto_increment not null unique,
+nome_album varchar(40), 
+primary key(cod_album),
+cod_bandaartista int,
+foreign key (cod_bandaartista) references Tb_Artista(cod_bandaartista)
+)default charset=utf8;
+
+create table Tb_Info_User(
+localizador_informacional int auto_increment not null unique,
+primary key(localizador_informacional),
+cod_user int, 
+foreign key (cod_user) references Tb_User(cod_user)
+)default charset=utf8;
+
+create table Tb_Download_Musica(
+cod_download_musica int auto_increment not null unique, 
+data_download_musica date not null, 
+horario_download_musica time not null, 
+cod_album int, 
+localizador_informacional int,
+foreign key(cod_album) references Tb_Album(cod_album),
+foreign key(localizador_informacional) references Tb_Info_User(localizador_informacional)
+)default charset=utf8;
+
+create table Tb_Follow_Artista(
+cod_follow_artista int auto_increment not null unique,
+data_follow_artista date not null, 
+hora_follow_artista time not null, 
+cod_bandaartista int, 
+localizador_informacional int, 
+foreign key (cod_bandaartista) references Tb_Artista(cod_bandaartista),
+foreign key(localizador_informacional) references Tb_Info_User(localizador_informacional)
+)default charset=utf8;
+
+create table Tb_Download_Album(
+cod_download_album int auto_increment not null unique, 
+data_download_album date not null, 
+hora_download_album time not null, 
+cod_album int unique,
+localizador_informacional int unique,
+foreign key(cod_album) references Tb_Album(cod_album),
+foreign key(localizador_informacional) references Tb_Info_User(localizador_informacional)
+)default charset=utf8;
 
