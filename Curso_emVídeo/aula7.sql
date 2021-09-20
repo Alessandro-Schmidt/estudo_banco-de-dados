e51ec55e3af6f11bb2863f5f362b150d9f3ddeb3
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-20 19:43:49 -0300

    Create aula7.sql

---
+++ Curso_emVídeo/aula7.sql
@@ -0,0 +1,20 @@
+/*
+Nesta aula iremos estudar sobre Manipulação de Linhas. 
+(UPDATE DELETE and TRUNCATE)
+*/
+
+select * from Gafanhotos; 
+select * from cursos; 
+
+insert into cursos values
+('12', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+('13','Logica', 'Lógica de programação', '20', '15','2014'),
+('14', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('15', 'PGP', 'Curso de PHP para iniciantes','40', '20', '2010'),
+('16', 'Jarva', 'Introdução a Linguagem Java','10', '29', '2000'),
+('17', 'MySQL Workbench', 'Banco de dados MySQL','30', '15', '2016'),
+('18', 'Word', 'Curso completo de Word','40', '30', '2016'),
+('19', 'Sapateado', 'Danças Rítmicas','40', '30', '2018'),
+('20', 'Cozinha Árabe', 'Aprenda a fazer comidas do oriente','40', '30', '2019'),
+('21', 'YouTuber', 'Gerando Intrigas com o YouTube','5', '2', '2018');
+
