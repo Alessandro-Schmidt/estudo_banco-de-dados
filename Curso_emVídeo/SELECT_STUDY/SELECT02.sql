ace33bfce287b66fd01df60a5c0b97cefd7e2d95
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-27 16:11:59 -0300

    Update SELECT02.sql

--- Curso_emVídeo/SELECT_STUDY/SELECT02.sql
+++ Curso_emVídeo/SELECT_STUDY/SELECT02.sql
@@ -11,3 +11,15 @@ where nome like '%a'; -- Todos que terminam com a letra a
 
 select * from cursos 
 where nome like '%a%';  -- A em qualquer lugar
+
+select * from cursos
+where nome like 'ph%p%'; -- Comece com PH, tenha p, e termine de qualquer outro jeito
+
+-- Lista de nomes não repetidos: 
+select distinct nacionalidade from gafanhotos
+order by nacionalidade; 
+
+-- Funções de agregações
+select count(*) from cursos; 
+
+select count(*) from cursos where carga > 40; 
