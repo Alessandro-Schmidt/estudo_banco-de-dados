ba3fee05136375e302c444273d56a2366b962c26
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-27 15:59:47 -0300

    Update SELECT02.sql

--- Curso_emVídeo/SELECT_STUDY/SELECT02.sql
+++ Curso_emVídeo/SELECT_STUDY/SELECT02.sql
@@ -8,3 +8,6 @@ where nome like 'P%'; -- P seguido de qualquer coisa
 -- Wildcards
 select * from cursos 
 where nome like '%a'; -- Todos que terminam com a letra a 
+
+select * from cursos 
+where nome like '%a%';  -- A em qualquer lugar
