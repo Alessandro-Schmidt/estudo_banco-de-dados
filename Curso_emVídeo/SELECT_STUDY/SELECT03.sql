879b8176b53858d75f265df7ef51e98b670a5a17
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-28 14:17:50 -0300

    Update SELECT03.sql

--- Curso_emVídeo/SELECT_STUDY/SELECT03.sql
+++ Curso_emVídeo/SELECT_STUDY/SELECT03.sql
@@ -11,3 +11,41 @@ group by carga;
 
 select carga, count(nome) from cursos where totaulas = 30
 group by carga;
+
+-- having 
+select ano, count(*) from cursos group by ano order by count(*) desc;
+
+select ano, count(*) from cursos 
+group by ano 
+having count(ano) >=5
+order by count(*) desc;
+
+select ano, count(*) from cursos 
+group by ano 
+having count(ano) >30 -- Não funciona
+order by count(*) desc;
+
+
+select ano, count(*) from cursos 
+group by ano 
+having ano>2013
+order by count(*) desc;
+
+select ano, count(*) from cursos 
+where totaulas>30
+group by ano 
+having ano>2013
+order by count(*) desc;
+
+select avg(carga) from cursos;
+
+select carga, count(*) from cursos 
+where ano > 2015
+group by carga
+having carga > (select avg(carga) from cursos); -- Só aparece quem tem uma carga maior que a média das cargas, para isso, emende um select em outro. 
+
+-- EX1 - Lista com todas as profissoes e seus quantitativos: 
+select distinct profissao, count(*) from gafanhotos 
+group by profissao; 
+
+-- EX2 - 
