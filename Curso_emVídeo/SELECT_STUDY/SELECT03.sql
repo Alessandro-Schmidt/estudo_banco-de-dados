aa8ca15629d88b700b4b4152f8726fbf3120840c
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-28 12:24:38 -0300

    Update SELECT03.sql

--- Curso_emVídeo/SELECT_STUDY/SELECT03.sql
+++ Curso_emVídeo/SELECT_STUDY/SELECT03.sql
@@ -3,3 +3,11 @@
 -- Distinguindo: o distinct considera apenas uma ocorrência dentro de cada tabela. Evita repetições
 
 -- Agrupando: Há repetições, mas os valores iguais estão sempre juntos. 
+
+-- Unindo os dois conceitos: 
+
+select carga, count(nome) from cursos 
+group by carga; 
+
+select carga, count(nome) from cursos where totaulas = 30
+group by carga;
