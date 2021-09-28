c5fd71f7e939f97220c7d3046422d6497e7beafe
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-28 15:32:54 -0300

    Update foreignkey&Join.sql

--- Curso_emVídeo/foreignkey&Join.sql
+++ Curso_emVídeo/foreignkey&Join.sql
@@ -19,6 +19,22 @@ from gafanhotos join cursos
 on cursos.idcurso = gafanhotos.cursopreferido; 
 
 select gafanhotos.nome, cursos.nome, cursos.ano
-from gafanhotos join cursos 
+from gafanhotos inner join cursos 
 on cursos.idcurso = gafanhotos.cursopreferido
 order by gafanhotos.nome; 
+
+-- Simplificando
+select g.nome, c.nome, c.ano
+from gafanhotos as g inner join cursos as c
+on c.idcurso = g.cursopreferido
+order by g.nome; 
+
+-- outer join
+select g.nome, c.nome, c.ano
+from gafanhotos as g left outer join cursos as c
+on c.idcurso = g.cursopreferido; -- Mostra alunos que não tem curso preferido, dando prioridade para a tabela da esquerda (gafanhotos); 
+
+select g.nome, c.nome, c.ano
+from gafanhotos as g right outer join cursos as c
+on c.idcurso = g.cursopreferido; -- Mostra os cursos que não tem alunos que gostam dele, da prioridade para a tabela da direita (cursos)
+
