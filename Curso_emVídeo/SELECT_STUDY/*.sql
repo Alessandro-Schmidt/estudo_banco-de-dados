ebda705eebeb369f93ff79ca06d44e8f7b46047d
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-30 19:16:51 -0300

    PRova Refeita

---
+++ Curso_emVídeo/SELECT_STUDY/*.sql
@@ -0,0 +1,71 @@
+-- Obtendo dados das tabelas: 
+
+-- Distinguindo: o distinct considera apenas uma ocorrência dentro de cada tabela. Evita repetições
+
+-- Agrupando: Há repetições, mas os valores iguais estão sempre juntos. 
+
+-- Unindo os dois conceitos: 
+
+use cadastro; 
+select carga, count(nome) from cursos 
+group by carga; 
+
+select carga, count(nome) from cursos where totaulas = 30
+group by carga;
+
+-- having 
+select ano, count(*) from cursos group by ano order by count(*) desc;
+
+select ano, count(*) from cursos 
+group by ano 
+having count(ano) >=5
+order by count(*) desc;
+
+select ano, count(*) from cursos 
+group by ano 
+having count(ano) >30 -- Não funciona
+order by count(*) desc;
+
+
+select ano, count(*) from cursos 
+group by ano 
+having ano>2013
+order by count(*) desc;
+
+select ano, count(*) from cursos 
+where totaulas>30
+group by ano 
+having ano>2013
+order by count(*) desc;
+
+select avg(carga) from cursos;
+
+select carga, count(*) from cursos 
+where ano > 2015
+group by carga
+having carga > (select avg(carga) from cursos); -- Só aparece quem tem uma carga maior que a média das cargas, para isso, emende um select em outro. 
+
+-- EX1 - Lista com todas as profissoes e seus quantitativos: 
+select distinct profissao, count(*) from gafanhotos 
+group by profissao; 
+
+-- EX2 - Lista de quantos homens e mulheres nasceram depois de 2005
+select sexo, count(*) from gafanhotos
+where nascimento > '2005-01-01'
+group by sexo; 
+
+-- EX3 - Lista dos países e quantos aluos existem, tirando o brasil
+select distinct nacionalidade, count(*) from gafanhotos
+where nacionalidade<>'Brasil'
+group by nacionalidade
+having count(nacionalidade)>3; 
+
+-- EX4 - Exercicio complexo demais
+select avg(altura) from gafanhotos; 
+
+-- Segunda parte: 
+select * from gafanhotos 
+where peso > 100
+having altura >(select avg(altura) from gafanhotos)
+order by altura; 
+
--- Universidade/SOMATIVA/ALESSANDRO SCHMIDT.sql
+++ Universidade/SOMATIVA/ALESSANDRO SCHMIDT.sql
@@ -8,11 +8,35 @@ create table paciente(
 numero_paciente int primary key, 
 nome_paciente varchar(20) not null
 ); 
+alter table paciente
+add column sobrenome varchar(20) after nome_paciente; 
+
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('1', 'Alessandro', 'Schmidt', '1');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('2', ' Magda', 'Abgail', '1');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('3', 'Alessandro', 'Maciel', '1');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('4', 'Heloísa', 'Lorenzet', '2');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('5', 'Ana', 'Raquel', '2');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('6', 'Magnus', 'Carlsen', '2');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('7', 'Alireza ', 'Firouja', '3');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('8', 'Maxime', 'Vachier', '3');
+INSERT INTO `hospital`.`paciente` (`numero_paciente`, `nome_paciente`, `sobrenome`, `convenio_paciente`) VALUES ('9', 'Ivan', 'Latouc', '3');
+
 
 create table medico(
 crm int primary key, 
 nome_medico varchar(20) not null
 );
+select * from medico; 
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1111', 'Adamastor', '1');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1112', 'Ana Maria', '1');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1113', 'Carlos', '2');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1114', 'Sabrina', '2');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1115', 'Thyerri', '1');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1116', 'Carolina', '2');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1117', 'Alessandro', '1');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1118', 'Ana Raquel', '3');
+INSERT INTO `hospital`.`medico` (`crm`, `nome_medico`, `convenio_medico`) VALUES ('1119', 'Agon', '2');
+
 
 create table telefone_paciente( -- Atributo multivalorado de telefone de paciente 
 cod_telefone int primary key, 
@@ -22,6 +46,15 @@ id_paciente int
 );
 alter table telefone_paciente
 add foreign key (id_paciente) references paciente(numero_paciente); 
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('1', '123123', 'TRABALHO', '1');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('2', '333333', 'RESIDENCIAL', '2');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('3', '444444', 'OUTRO', '3');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('4', '555555', 'OUTRO', '4');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('5', '667667', 'RESIDENCIAL', '5');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('6', '776776', 'TRABALHO', '6');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('7', '888888', 'RESIDENCIAL', '7');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('8', '999999', 'OUTRO', '8');
+INSERT INTO `hospital`.`telefone_paciente` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_paciente`) VALUES ('9', '191919', 'OUTRO', '9');
 
 create table telefone_medico( -- Atributo multivalorado de telefone do médico
 cod_telefone int primary key,
@@ -29,9 +62,17 @@ telefone int,
 tipo_telefone enum('RESIDENCIAL', 'TRABALHO', 'OUTRO'), 
 id_medico int
 );
-
 alter table telefone_medico
 add foreign key (id_medico) references medico(crm); 
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('1', '111111', 'OUTRO', '1111');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('2', '212333', 'RESIDENCIAL', '1112');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('3', '199999', 'TRABALHO', '1113');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('4', '222000', 'TRABALHO', '1114');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('5', '1111222', 'TRABALHO', '1115');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('6', '1212', 'OUTRO', '1116');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('7', '444222', 'RESIDENCIAL', '1117');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('8', '2112', 'OUTRO', '1118');
+INSERT INTO `hospital`.`telefone_medico` (`cod_telefone`, `telefone`, `tipo_telefone`, `id_medico`) VALUES ('9', '5556777', 'OUTRO', '1119');
 
 
 create table consulta(
@@ -42,19 +83,47 @@ numero_paciente int
 );
 alter table consulta
 add foreign key (crm_medico) references medico(crm); 
-
 alter table consulta
 add foreign key (numero_paciente) references paciente(numero_paciente);
+
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('1', '1111', '2');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('2', '1111', '1');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('3', '1111', '3');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('4', '1111', '4');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('5', '1111', '5');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('6', '1112', '6');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('7', '1113', '7');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('8', '1114', '8');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('9', '1119', '9');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('10', '1111', '9');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('11', '1111', '8');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('12', '1114', '1');
+INSERT INTO `hospital`.`consulta` (`cod_consulta`, `crm_medico`, `numero_paciente`) VALUES ('13', '1116', '1');
+
+
 
 create table exame_recomendado( -- Atributo multivalorado, vira tabela na normalização
 cod_exame_recomendado int primary key, 
 exame varchar(90),
 cod_consulta int 
 );
-
-
 alter table exame_recomendado
 add foreign key (cod_consulta) references consulta(cod_consulta);
+
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('1', 'Ultrasom', '1');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('2', 'Radioterapia', '2');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('3', 'Ultrasom', '3');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('4', 'Radioterapia', '4');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('5', 'Holter', '5');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('6', 'Radioterapia', '6');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('7', 'Ultrasom', '7');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('8', 'Radioterapia', '8');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('9', 'Holter', '8');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('10', 'Holter', '1');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('11', 'Radioterapia', '12');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('12', 'Mapa', '11');
+INSERT INTO `hospital`.`exame_recomendado` (`cod_exame_recomendado`, `exame`, `cod_consulta`) VALUES ('13', 'Mapa', '10');
+
 
 create table medicamento_receitado( -- Atributo multivalorado, vira tabela na normalização
 cod_medicamento_recomendado int primary key, 
@@ -64,6 +133,18 @@ cod_consulta int
 alter table medicamento_receitado
 add foreign key (cod_consulta) references consulta(cod_consulta);
 
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('1', 'Ibuprofeno', '1');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('2', 'Paracetamol', '1');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('3', 'Antialérgico', '2');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('4', 'Vitamina D', '4');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('5', 'Antialérgico', '5');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('6', 'Beta Bloqueador', '6');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('7', 'Melatonina', '7');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('8', 'Beta Bloqueador', '7');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('9', 'Antialérgico', '9');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('10', 'Beta Bloqueador', '10');
+INSERT INTO `hospital`.`medicamento_receitado` (`cod_medicamento_recomendado`, `medicamento_receitado`, `cod_consulta`) VALUES ('11', 'Beta Bloqueador', '12'); 
+
 create table diagnostico(
 cod_diagnostico int primary key,
 diagnostico varchar(150),
@@ -72,11 +153,27 @@ cod_consulta int
 alter table diagnostico
 add foreign key (cod_consulta) references consulta(cod_consulta);
 
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('1', 'Muito bom prognóstico', '1');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('2', 'Precisa ver um especialista', '2');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('3', 'Muito bom prognóstico', '3');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('4', 'Precisa de cirurgia', '4');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('5', 'Precisa ver outro médico', '5');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('6', 'Precisa ver outro médico', '6');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('7', 'Muito bom prognóstico', '7');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('8', 'Precisa de cirurgia', '8');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('9', 'Precisa de cirurgia', '9');
+INSERT INTO `hospital`.`diagnostico` (`cod_diagnostico`, `diagnostico`, `cod_consulta`) VALUES ('10', 'Muito bom prognóstico', '10');
+
 
 create table convenio(
 cod_convenio int primary key, 
 nome varchar(20) not null
 );
+INSERT INTO `hospital`.`convenio` (`cod_convenio`, `nome`) VALUES ('1', 'Unimed');
+INSERT INTO `hospital`.`convenio` (`cod_convenio`, `nome`) VALUES ('2', 'IPE');
+INSERT INTO `hospital`.`convenio` (`cod_convenio`, `nome`) VALUES ('3', 'PUCPR');
+INSERT INTO `hospital`.`convenio` (`cod_convenio`, `nome`) VALUES ('4', 'SICREDI');
+INSERT INTO `hospital`.`convenio` (`cod_convenio`, `nome`) VALUES ('5', 'AgroBrasil');
 
 alter table medico
 add column convenio_medico int; 
@@ -99,7 +196,7 @@ Garantir que a terceira exija o operador da álgebra relacional diferença
 
 R: 
 1ª - Qual o médico com mais consultas registradas? 
-2ª - Qual o convênio que mais envia pacientes para consultas? 
+2ª - Qual o convênio do médico com mais consultas registradas? 
 3ª - Além do exame de ultrasom, qual é o exame mais recomendado por médicos em consultas? 
 */
 
@@ -115,12 +212,16 @@ group by c.crm_medico;
 
 
 -- 2ª QUESTÃO
-select distinct num_paciente, count(cod_consulta) from consulta;
+select m.crm, m.nome_medico, conv.nome, count(c.crm_medico)
+from medico as m inner join convenio as conv
+on m.convenio_medico = conv.cod_convenio
+join consulta as c on c.crm_medico = m.crm
+group by m.crm
+order by count(c.crm_medico) desc;
 
-select conv.nome, count(cons.cod_consulta)
-from convenio as conv inner join consulta as cons
 
-
-
-
+-- 3ª QUESTÃO
+select exame, count(exame) from exame_recomendado
+where exame<>'Ultrasom'
+group by exame desc;
 
